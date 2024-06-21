b8dca032c755a4779093d8033ff041163a5fa185
jupivato <juliapivato04@gmail.com>
2024-06-21 11:33:30 -0300

    lexer

--- lexer.py
+++ lexer.py
@@ -1,73 +1,84 @@
-#import da biblioteca ply para a construção dos tokens
+# import da biblioteca ply para a construção dos tokens
 import ply.lex as lex
 
-#lista de tokens
+# Tokens fornecidos
 tokens = [
     'TIPO_INT', 'TIPO_FLOAT', 'TIPO_CHAR',
+    'VARIAVEL',
     'ATRIBUICAO', 'COLON',
-    'SOMA', 'SUBTRACAO', 'MULTIPLICACAO', 'DIVISAO', 'RESTO',
+    'SOMA', 'MAISMAIS', 'SUBTRACAO', 'MENOSMENOS', 'MULTIPLICACAO', 'DIVISAO', 'RESTO',
     'PONTO', 'PONTO_VIRGULA', 'VIRGULA',
     'E', 'OU', 'NEGACAO',
     'IGUAL', 'MAIOR_IGUAL', 'MENOR_IGUAL', 'MAIOR', 'MENOR', 'DIFERENTE',
     'ABRE_PARENTESES', 'FECHA_PARENTESES',
-    'INICIO', 'FIM', 'ENTRADA', 'SAIDA', 'COND_IF', 'COND_ELSE', 'REP_DURING', 'REP_THROUGH', 'BLOCO_DE_COMANDO'
+    'INICIO', 'FIM',
+    'ENTRADA', 'SAIDA',
+    'COND_IF', 'COND_ELSE', 'REP_DURING', 'REP_THROUGH',
+    'BLOCO_DE_COMANDO'
 ]
 
-#expressões regulares para tokens simples
-t_ATRIBUICAO = r'='
+# Expressões regulares para tokens simples
+t_ATRIBUICAO = r'<-'
 t_SOMA = r'\+'
+t_MAISMAIS = r'\+\+' 
 t_SUBTRACAO = r'-'
+t_MENOSMENOS = r'--' 
 t_MULTIPLICACAO = r'\*'
 t_DIVISAO = r'/'
 t_RESTO = r'%'
-t_E = r'and'
-t_OU = r'or'
-t_NEGACAO = r'not'
-t_IGUAL = r'=='
+t_E = r'&'
+t_OU = r'\|'
+t_NEGACAO = r'\#'
+t_IGUAL = r'\='
 t_MAIOR_IGUAL = r'>='
 t_MENOR_IGUAL = r'<='
 t_MAIOR = r'>'
 t_MENOR = r'<'
-t_DIFERENTE = r'!='
+t_DIFERENTE = r'\#='
 t_ABRE_PARENTESES = r'\('
 t_FECHA_PARENTESES = r'\)'
 t_VIRGULA = r','
 
+
 #palavras reservadas
 reserved = {
-    'init': 'INIT',
-    'pinguim': 'END',
-    'pin': 'PIN',
-    'pout': 'POUT',
-    'pif': 'PIF',
-    'paf': 'PAF',
-    'phile': 'PHILE',
-    'pegin': 'PEGIN',
-    'pend': 'PEND'
+    'int': 'TIPO_INT',
+    'float': 'TIPO_FLOAT',
+    'char': 'TIPO_CHAR',
+    'in': 'INICIO',
+    'out': 'FIM',
+    'if': 'COND_IF',
+    'else': 'COND_ELSE',
+    'through': 'REP_THROUGH',
+    'during': 'REP_DURING',
+    'start': 'INICIO',
+    'end': 'FIM'
 }
 
 #tokens com regras mais complexas
-def t_ID(t):
-    r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t.type = reserved.get(t.value, 'ID')
+def t_VARIAVEL(t):
+    r'[a-zA-Z][a-zA-Z0-9]*'
+    t.type = reserved.get(t.value, 'VARIAVEL')
     return t
 
-def t_REAL(t):
+def t_FLOAT(t):
     r'[-]?[0-9]+\.[0-9]+'
     t.value = float(t.value)
     return t
 
-def t_NUMBER(t):
+def t_INT(t):
     r'[-]?[0-9]+'
     t.value = int(t.value)
     return t
 
-def t_STRING(t):
-    r'\"([^\\\n]|(\\.))*?\"'
-    t.value = str(t.value)
+def t_CHAR(t):
+    r"\"([^\\'\n]|(\\.)|())\""
+    t.value = t.value[1:-1]
     return t
 
-#ignorar espaços e tabulações (não são tokens)
+
+
+#ignorar espaços e tabulações
 t_ignore = ' \t'
 
 #definir comportamento para novas linhas (incrementar o número da linha)
